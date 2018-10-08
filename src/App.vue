f43aaa114ea5683688ecd540b371ddb80b46afc7
jeff-beagley <jeff-beagley@mihlfeld.com>
2018-10-08 00:39:24 -0500

    Swapped to vlue cli 3

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,19 @@
 <template>
   <div id="app">
- <b-container>
-        <b-jumbotron header="Bootstrap Vue"
-                     lead="Bootstrap 4 Components for Vue.js 2"
-        >
-          <p>For more information visit our website</p>
-          <b-btn variant="primary" href="https://bootstrap-vue.js.org/">More Info</b-btn>
-        </b-jumbotron>
-
-        <b-form-group horizontal
-                      :label-cols="4"
-                      description="Let us know your name."
-                      label="Enter your name"
-        >
-           <b-form-input v-model.trim="name"></b-form-input>
-        </b-form-group>
-
-        <b-alert variant="success" :show="showAlert">
-          Hello {{ name }}
-        </b-alert>
-      </b-container>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'app',
-        el: "#app",
-        data: {
-          name: ''
-        },
-        computed: {
-          showAlert() {
-            return this.name.length > 4 ? true : false;
-          }
-        }
+  components: {
+    HelloWorld
   }
-
+}
 </script>
 
 <style>
@@ -47,23 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
